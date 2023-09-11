da4d6053638fb3c38495ba8dbbbb9a2e59a9729f
Juan Manuel CastillO <juanmcastillo3@gmail.com>
2023-09-10 22:15:17 -0300

    to do list

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-let idCounter = 0;
+let IdCounter = 0;
+const input = document.querySelector('input[type="text"]');
 
-userInput.addEventListener('submit', (event) =>{
+userInput.addEventListener('submit', (event)=>{
     event.preventDefault();
-    addTask();
+    if(input.value.trim().length != 0){
+        addTask();
+    }
 });
 
-let addTask = ()=> {
-    idCounter++;
-    list.innerHTML += ′
-    <div class="task-container" id="${idCounter}">
-      <label>
-        <input type="checkbox" />
-        Tarea 1
-      </label>
-      <img src="./images/borrar.png" class="closeBtn">
-    </div>
-  </div>
-    ′
+let addTask = ()=>{
+    IdCounter++;
+
+    //Recoger Texto
+    let newValue = input.value;
+
+    //Agregar tarea
+    const newTask = `
+        <div class="task-container" id="${IdCounter}">
+            <label>
+                <input type="checkbox"> 
+                ${newValue}
+            </label>
+            <img src="./images/borrar.png" class="closeBtn">
+        </div>
+    `;
+    list.innerHTML += newTask;
+    input.value = '';
+    updateStats();
+}
+
+list.addEventListener('click', (event)=>{
+    if(event.srcElement.nodeName == 'INPUT'){
+        updateStats();
+    }else if(event.srcElement.nodeName == 'IMG'){
+        deleteTask(event.srcElement.parentNode.id);
+    }
+});
+
+let updateStats = ()=>{
+    let element = list.querySelectorAll('div');
+    let checkbox = document.querySelectorAll('input[type="checkbox"]:checked');
+    stats.innerHTML = `Tareas pendientes: ${element.length} Completadas: ${checkbox.length}`;
+};
+
+let deleteTask = (id)=>{
+    let taskToDelete = document.getElementById(id);
+    list.removeChild(taskToDelete);
+    updateStats();
 };
